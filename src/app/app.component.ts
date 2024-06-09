e2e387fd1599d972ee5b192af2098fe5dca99a06
tarunkovoornd <taruntkovoor@gmail.com>
2024-06-08 21:37:25 -0400

    Added 10 sample practice questions. Click on the Practice button to view it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { QuestionComponent } from './question/question.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, QuestionComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'notre-dame-math-test';
+  showQuestions = false;
+
+  onPracticeNowClick() {
+    this.showQuestions = true;
+  }
 }
